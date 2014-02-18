354eb524280d2c5b41adc0b86288ca66040d8a30
pgivens4915 <pgivens4915@gmail.com>
2014-02-17 18:29:42 -0700

    Reading line by line

--- csvToMap.rs
+++ csvToMap.rs
@@ -1,3 +1,13 @@
+use std::io::buffered::BufferedReader;
+use std::io::File;
+
 fn main(){
   println!("Start");
+  let path = Path::new("input.txt");
+  let mut file = BufferedReader::new(File::open(&path));
+  let lines :  ~[~str] = file.lines().collect();
+
+  for line in lines.iter() {
+    print(*line);
+  }
 }
