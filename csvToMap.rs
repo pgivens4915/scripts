9cb397fd6c615d9c45c9ce4eb215e6e1fcdcf6d0
pgivens4915 <pgivens4915@gmail.com>
2014-02-17 19:30:24 -0700

    Added a head and tail call

--- csvToMap.rs
+++ csvToMap.rs
@@ -2,12 +2,17 @@ use std::io::buffered::BufferedReader;
 use std::io::File;
 
 fn main(){
-  println!("Start");
+  // File IO
   let path = Path::new("input.txt");
   let mut file = BufferedReader::new(File::open(&path));
   let lines :  ~[~str] = file.lines().collect();
 
-  for line in lines.iter() {
+  // Removing the longitudes
+  let longitudes : &~str = lines.head();
+  let data : &[~str] = lines.tail();
+
+  // The main work
+  for line in data.iter() {
     let chunks : ~[&str] = line.split(',').collect();
     println(chunks[0]);
     for chunk in chunks.iter() {
